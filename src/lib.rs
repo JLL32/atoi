1dd1c8fefd974e898370ee2275f1a2f8c27dd19c
Abdeljalil El hachimi <ael-hach@e1r8p8.1337.ma>
2020-11-18 13:31:07 +0100

    initial

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use std::i32;
+
+pub fn atoi(s: &str) -> i32 {
+    if s.trim().is_empty() {
+        return 0;
+    }
+    match s.split_whitespace().collect::<Vec<_>>()[0].parse::<f64>() {
+        Ok(val) => match val {
+            d if d < (i32::MIN as f64) => i32::MIN,
+            d if d > (i32::MAX as f64) => i32::MAX,
+            _ => val as i32,
+        },
+        Err(_) => 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        assert_eq!(atoi("42"), 42);
+    }
+    #[test]
+    fn test2() {
+        assert_eq!(atoi("-42"), -42);
+    }
+    #[test]
+    fn test3() {
+        assert_eq!(atoi("      42"), 42);
+    }
+    #[test]
+    fn test4() {
+        assert_eq!(atoi("4193 with words"), 4193);
+    }
+    #[test]
+    fn test5() {
+        assert_eq!(atoi("words and 987"), 0);
+    }
+    #[test]
+    fn test6() {
+        assert_eq!(atoi("-91283472332"), i32::MIN);
+    }
+    #[test]
+    fn test7() {
+        assert_eq!(atoi("91283472332"), i32::MAX);
+    }
+    #[test]
+    fn test8() {
+        assert_eq!(atoi(""), 0);
+    }
+    #[test]
+    fn test9() {
+        assert_eq!(atoi("    "), 0);
+    }
+    #[test]
+    fn test10() {
+        assert_eq!(atoi("13.73"), 13);
+    }
+    #[test]
+    fn test11() {
+        assert_eq!(atoi("1337leet"), 1337);
+    }
+}
